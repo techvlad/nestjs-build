fdef45f2176478c740736a21f67af52176d81656
Peter Gundel <mail@petergundel.de>
2021-07-08 14:20:07 +0200

    Fix TypeORM loading in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { UsersService } from './users.service'
-import { UserRepository } from './user.repository'
+import { User } from './user.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   exports: [UsersService],
 })
